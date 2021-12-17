8f556f8dadae6f199a5a1422af097fac50e5f4a5
unofficialdxnny <82535503+unofficialdxnny@users.noreply.github.com>
2021-12-17 16:47:01 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
+# made by unofficialdxnny
+# @unofficialdxnny on Instagram
+# dont repost without giving credits (dont be noob)
+
+
+
+
 from instapy import InstaPy
 from selenium import webdriver;
 
 
-browser= webdriver.Firefox();
+browser= webdriver.Firefox(); # opens FireFox browser (you will neeed to install it if you haveent already)
 browser.get('http://www.instagram.com');
 
 
-session = InstaPy(username="unofficialdxnny", password="husban1snaughtydaniadanial123321")
+session = InstaPy(username="", password="") # insert your creedentials here
 session.login()
-session.like_by_tags(["javascript", "html", "css", "code", "java"], amount=5)
-session.set_dont_like(["naked", "nsfw"])
+session.like_by_tags(["javascript", "html", "css", "code", "java"], amount=5) # this is the tags(#) that the bot will search for
+session.set_dont_like(["naked", "nsfw"])# the tags that will be ignored :)
 session.set_do_follow(True, percentage=50)
 session.set_do_comment(True, percentage=50)
-session.set_comments(["Nice!", "Cool"])
-session.end()
+session.set_comments(["Nice!", "Cool"]) # the stuff the bot will comment
+session.end() # ends the session :)
